a262ff69fa6188d1e2f278888e5e53536b5ea035
masabmunir <masabk214@gmail.com>
2024-12-13 16:54:42 +0500

    working on project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { SalesComponent } from './sales/sales.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        pathMatch:'full',
+        redirectTo:'home'
+    },
+    {
+        path:'',
+        component:LayoutComponent,
+        children:[
+            {path:'home',
+                component:HomeComponent
+            },
+            {path:'sales',
+                component:SalesComponent
+            }
+        ]
+    }
+];
