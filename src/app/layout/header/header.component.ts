b19e45fc2e277d762d87cf85eaa335d390b074df
masabmunir <masabk214@gmail.com>
2024-12-26 17:00:58 +0500

    completed header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -3,16 +3,17 @@ import { EventEmitter } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule,MatIconModule,MatMenuModule],
+  imports: [MatToolbarModule,MatIconModule,MatMenuModule,MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
-  @Output() menuToggled = new EventEmitter<void>();
+  @Output() menuToggled = new EventEmitter<boolean>();
 
   user: string = 'Masab';
 
